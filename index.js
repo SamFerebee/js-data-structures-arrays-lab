e53fbde9ee0ba3bc90f7e4499069b6e4ff88753a
Sam Ferebee <sam.ferebee@gmail.com>
2021-02-22 02:12:24 -0500

    commit tree 230d7883817a17a1e8694b8ae2b1924be23d7c97

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+function appendDriver(name){
+    let newArr = [...drivers, name];
+    return newArr;
+}
+
+function prependDriver(name){
+    let newArr = [name, ...drivers];
+    return newArr;
+}
+
+function removeLastDriver(){
+    let arr1 = drivers.slice(0, -1);
+    return arr1;
+}
+
+function removeFirstDriver(){
+    let arr1= drivers.slice(1);
+    return arr1;
+}
